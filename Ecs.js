e5a8daf895a45132cf8ef122b0e7a25923e0f739
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2018-02-20 22:33:52 +0200

    Update readme

--- Ecs.js
+++ Ecs.js
@@ -1,313 +1,108 @@
-var Ecs = function(){
-	var idCounter,
-		components,
-		systems,
-		groups,
-		entities,
-		garbage;
+function Ecs() {
+	let entities = [];
+	let components = {};
+	let systems = [];
+	let idCounter = 1;
 
-	function Ecs(){
-		this.init();
+	function Entity() {};
+
+	Entity.prototype = {
+		add(name, ...args) {
+			this[name] = new components[name](...args);
+			return this;
+		},
+		remove(name) {
+			delete this[name];
+			return this;
+		},
+		has(...keys) {
+			return keys.every(key => !!this[key]);
+		},
+		destroy() {
+			this._garbage = true;
+			entities = entities.filter(entity => entity !== this);
+		},
+		matches(system) {
+			return system.has.every(key => this.has(key)) && system.not.every(key => !this.has(key));
+		},
+		isActive() {
+			return !this._garbage;
+		}
 	};
 
-	Ecs.prototype.init = function(){
-		idCounter = 0;
-		entities = [];
-		components = {};
-		groups = {};
-		systems = [];
+	function System({
+		mounted = noop,
+		pre = noop,
+		post = noop,
+		forEach = noop,
+		has = [],
+		not = [],
+		order = 0,
+	}) {
+		this.mounted = mounted;
+		this.pre = pre;
+		this.post = post;
+		this.forEach = forEach;
+		this.has = has;
+		this.not = not;
+		this.order = order;
 	};
 
-	Ecs.prototype.entity = function(){
-		var entity = new Entity();
+	System.prototype = {
+		run(global) {
+			this.entities = this.getMatchingEntities();
+			this.pre(global);
+			this.entities.forEach(entity => entity.isActive() && this.forEach(entity, global));
+			this.post(global);
+		},
+		getMatchingEntities() {
+			return entities.filter(entity => entity.isActive() && entity.matches(this));
+		},
+		getOtherEntities(currentEntity) {
+			return entities.filter(entity => entity.isActive() && entity !== currentEntity);
+		}
+	};
+
+	function createEntity() {
+		const entity = new Entity();
+		Object.defineProperty(entity, 'id', {
+			enumerable: false,
+			value: idCounter++
+		});
 		entities.push(entity);
 		return entity;
 	};
 
-	Ecs.prototype.system = function(options){
-		var system = new System(options);
+	function registerComponent(name, component = defaultComponent) {
+		components[name] = component;
+		return name;
+	};
+
+	function registerSystem(conf) {
+		const system = new System(conf);
 		systems.push(system);
+		systems = systems.sort((a, b) => a.order - b.order);
+		system.mounted();
 		return system;
 	};
 
-	Ecs.prototype.component = function(name,fn){
-		if(components[name]) throw new Error("Overwriting already existing component: "+name)
-		components[name] = fn || function(value){this.value = value;};
+	function run(globalArg) {
+		systems.forEach(s => s.run(globalArg));
 	};
 
-	Ecs.prototype.removeEntity = function (id) {
-		for (var i = 0; i < entities.length; i++) {
-			if(entities[i].id === id){
-				entities[i]._disabled = true;
-				garbage = true;
-				break;
-			}
-		}
-	};
-
-	Ecs.prototype.run = function(globalArgs){
-		if(garbage) clearGarbage();
-		var i;
-		var len = systems.length;
-		for(i = 0; i < len; i++){
-			systems[i].run.call(systems[i],globalArgs);
-		}
-	};
-
-	Ecs.prototype.runGroup = function(name,globalArgs){
-		if(garbage) clearGarbage();
-		if(groups[name]){
-			var i;
-			var len = groups[name].length;
-			for(i = 0; i < len; i++){
-				groups[name][i].run.call(groups[name][i],globalArgs);
-			}
-		}
-	};
-
-	Ecs.prototype.getEntities = function(){
-		return entities;
-	};
-
-	Ecs.prototype.getEntityById = function (id) {
-		for (var i = 0; i < entities.length; i++) {
-			if(id === entities[i].id) return entities[i];
-		}
+	function getMatchingSystems(entity) {
+		return systems.filter(system => entity.matches(system));
 	}
 
-	Ecs.prototype.iterateAll = function (cb) {
-		var len = entities.length;
-		for (var i = 0; i < len; i++) {
-			cb(entities[i]);
-		}
+	function runGroup(name, globalArg) {
+		systems.filter(system => system.group === name).forEach(system => system.run(globalArg));
 	};
 
-	Ecs.prototype.emit = function () {
-		var name = arguments[0];
-		var args = Array.prototype.slice.call(arguments);
-		args.shift();
-		for (var i = 0; i < systems.length; i++) {
-			if(systems[i].subscriptions[name]){
-				systems[i].subscriptions[name].apply(systems[i], args);
-			}
-		}
-	};
-
-	function Entity(){
-		this.components = {};
-		this.id = idCounter++;
-	};
-
-	Entity.prototype.add = function(){
-		var name = arguments[0];
-		var args = Array.prototype.slice.call(arguments);
-		// this.components[name] = {};
-		if(!components[name]) throw new Error("Component doesn't exist: "+name)
-		this.components[name] = new (Function.prototype.bind.apply(components[name], args));
-		onAddComponent(this);
-		return this;
-	};
-
-	Entity.prototype.remove = function(name){
-		delete this.components[name];
-		onRemoveComponent(this);
-		return this;
-	};
-
-	Entity.prototype.has = function(name){
-		return !!this.components[name];
-	};
-
-	Entity.prototype.destroy = function () {
-		this._disabled = true;
-		garbage = true;
-	};
-
-	function clearGarbage() {
-		entities = entities.filter(function (e) {
-			return !e._disabled;
-		});
-		for (var i = 0; i < systems.length; i++) {
-			systems[i].entities = systems[i].entities.filter(function (e) {
-				return !e._disabled;
-			})
-		}
-		garbage = false;
+	function defaultComponent(v) {
+		this.value = v;
 	}
 
-	function onAddComponent(entity){
-		var i;
-		var len = systems.length;
-		var has;
-		for(i = 0; i < len; i++){
-			if(systems[i].hasEntity(entity) && systems[i].entityHasNot(entity)){//did we add component that is in "not" list?
-				systems[i].removeEntity(entity);
-			}
-			if(!systems[i].hasEntity(entity) && systems[i].entityHasComponents(entity) && !systems[i].entityHasNot(entity)){//did we add component that suffices to a new system?
-				systems[i].addEntity(entity);
-			}
-		}
-	};
+	function noop() {};
 
-	function onRemoveComponent(entity){
-		var i;
-		var len = systems.length;
-		for(i = 0; i < len; i++){
-			if(systems[i].hasEntity(entity) && !systems[i].entityHasComponents(entity)){//did we remove component that was needed for this system
-				systems[i].removeEntity(entity);
-			}
-			if(!systems[i].hasEntity(entity) && systems[i].entityHasComponents(entity) && !systems[i].entityHasNot(entity)){//did we remove "not" listed component and suffice to list?
-				systems[i].addEntity(entity);
-			}
-		}
-	};
-
-	function System(options){
-		var key;
-		for(key in options){
-			if(options.hasOwnProperty(key)){
-				this[key] = options[key];
-			}
-		}
-
-		if(this.group !== undefined){
-			groups[this.group] = groups[this.group]? groups[this.group] : [];
-			groups[this.group].push(this);
-		}
-		this.subscriptions = {};
-		this.entities = [];
-    if(options.init) options.init.call(this);
-	};
-
-	System.prototype.run = function(globalArgs){
-		if(this.pre){
-			this.pre.apply(this,arguments);
-		}
-    if(!this.every) return;
-    var args;
-		var i;
-		var len = this.entities.length;
-		for(i = 0; i < len; i++){
-			if(this.entities[i] && !(this.entities[i]._disabled)){
-				this._currentEntity = this.entities[i];
-				args = this.getArguments(this.entities[i]);
-				if(globalArgs) args.push(globalArgs);
-				this.every.apply(this,args);
-			}
-		}
-		if(this.post){
-			this.post.apply(this,arguments);
-		}
-		this._currentEntity = null;
-	};
-
-	System.prototype.getArguments = function(entity){
-		if(!this.components) return false;
-		var args = [];
-		var i;
-		var len = this.components.length;
-		for(i = 0; i < len; i++){
-			args.push(entity.components[this.components[i]]);
-		}
-		args.push(entity);
-		return args;
-	};
-
-	System.prototype.hasEntity = function(entity){
-		var i;
-		var len = this.entities.length;
-		for(i = 0; i < len; i++){
-			if(this.entities[i].id === entity.id) return true;
-		}
-		return false;
-	};
-
-	System.prototype.removeEntity = function(entity){
-		if(this.onleave){
-			this.onleave.apply(entity,this.getArguments(entity));
-		}
-		var i;
-		var len = this.entities.length;
-		for(i = 0; i < len; i++){
-			if(this.entities[i].id === entity.id){
-				this.entities.splice(i,1);
-				return true;
-			}
-		}
-		return false;
-	};
-
-	System.prototype.addEntity = function(entity){
-		this.entities.push(entity);
-		if(this.onenter){
-			this.onenter.apply(entity,this.getArguments(entity));
-		}
-	};
-
-	System.prototype.entityHasComponents = function(entity){
-		if(!this.components) return true;
-		var i;
-		var len = this.components.length;
-		for(i = 0; i < len; i++){
-			if(!entity.components[this.components[i]]){
-				return false;
-			}
-		}
-		return true;
-	};
-
-	System.prototype.entityHasNot = function(entity){
-		if(!this.not) return false;
-		var i;
-		var len = this.not.length;
-		for(i = 0; i < len; i++){
-			if(entity.components[this.not[i]]){
-				return true;
-			}
-		}
-		return false;
-	};
-
-	System.prototype.iterate = function (cb) {
-		var args;
-		var i;
-		var len = this.entities.length;
-		for(i = 0; i < len; i++){
-			if(this.entities[i] && !this.entities[i]._disabled){
-				args = this.getArguments(this.entities[i]);
-				cb.apply(this.entities[i],args);
-			}
-		}
-	};
-
-	System.prototype.iterateOthers = function (cb) {
-		var args;
-		var i;
-		var len = this.entities.length;
-		for(i = 0; i < len; i++){
-			if(this.entities[i] && !(this.entities[i]._disabled)){
-				if(this.entities[i].id !== this._currentEntity.id){
-					args = this.getArguments(this.entities[i]);
-					cb.apply(this.entities[i],args);
-				}
-			}
-		}
-	};
-
-	System.prototype.emit = function () {
-		var name = arguments[0];
-		var args = Array.prototype.slice.call(arguments);
-		args.shift();
-		for (var i = 0; i < systems.length; i++) {
-			if(systems[i].subscriptions[name]){
-				systems[i].subscriptions[name].apply(systems[i], args);
-			}
-		}
-	};
-
-	System.prototype.on = function (name,cb) {
-		this.subscriptions[name] = cb;
-	}
-
-	return new Ecs();
-};
+	return { createEntity, registerComponent, registerSystem, run, runGroup, entities };
+}
--- README.md
+++ README.md
@@ -1,39 +1,38 @@
 # Entity Component System
-Ecs is an implementation of entity component system architecture in JavaScript. Ecs architecture helps to keep the logic and the data separated.
+Ecs is a lightweight JS library that can be used to implement an entity-component-system architecture for games and other similar applications. Ecs doesn't try to be a game engine but it can be used in tandem with other libraries to build complete games. The core idea of Ecs is to enable better `composition over inheritance` thus helping you to focus on developing different behaviours and then composing the behaviours to create the actual game objects.
 ## How to install
 ```html
 <script src="Ecs.js"></script>
 ```
 ## Examples
-Create an instance of Ecs.
+Create an instance of Ecs. There can be multiple parallel instances of Ecs existing at once.
 ```javascript
-var ecs = new Ecs();
+const ecs = new Ecs();
 ```
 ### Component
 Define components to hold data.
 ```javascript
-ecs.component("name", function(first, last) {
+ecs.registerComponent("name", function(first, last) {
   this.first = first;
   this.last = last;
-  this.full = first + " " + last;
 });
 
-ecs.component("sprite", function(src) {
+ecs.registerComponent("sprite", function(src) {
   this.src = src;
 });
 
-ecs.component("position", function(x, y) {
+ecs.registerComponent("position", function(x, y) {
   this.x = x;
   this.y = y;
 });
 
-ecs.component("velocity", function(x, y, max) {
+ecs.registerComponent("velocity", function(x, y, max) {
   this.x = x;
   this.y = y;
   this.max = max;
 });
 
-ecs.component("input", function() {
+ecs.registerComponent("input", function() {
   this.UP = false;
   this.DOWN = false;
   this.LEFT = false;
@@ -41,48 +40,120 @@ ecs.component("input", function() {
   this.SPACE = false;
 });
 
-ecs.component("playerControlled");
+ecs.registerComponent("playerControlled");
 
-ecs.component("hidden");
+ecs.registerComponent("hidden");
 ```
 ### Entity
-Entities are simple objects and they have two properties: `id` [int] and `components` [object].
+Entities store components and unique id.
 ```javascript
-var player = ecs.entity();
+const player = ecs.createEntity();
 ```
 Add and remove components from entity. Adding and removing of entities can be chained.
 ```javascript
-var player = ecs.entity()
-  .add("position", 50, 50)//add components to entity
+const player = ecs.createEntity()
+  .add("position", 50, 50)
   .add("velocity",0 ,0)
   .add("input");
 
-player.remove("velocity");//remove components from entity
+//later in code
+player.remove("velocity");
 ```
-Test if entity has a specific component.
+An entity can test existence of a component.
 ```javascript   
+const player = ecs.createEntity()
+  .add("position", 50, 50);
+
 player.has("position");//true
 player.has("velocity");//false
 ```
-Individual components and their data can be accessed through the `components` property of an entity.
+Individual components and their properties can be accessed directly as a property of the entity.
 ```javascript
-player.components.position;//{x:50,y:50}
+player.position;//{ x: 50, y: 50 }
 ```
-Entities have unique id.
+Entities have unique id. The id is not enumerable which enables simple iteration of components of an entity.
 ```javascript
-var ent = ecs.entity();//ent.id is a unique integer
+const player = ecs.createEntity();
+
+console.log(player.id);//--> 1
 ```
 ### System
-Define systems to implement logic. Systems process entities that have all components listed in the `components` array.
+Register systems to implement logic. Systems process entities that match their required components. `has` is an array listing all required components for an entity to be enrolled to the system. `not`is an array that can be used to exclude those entities that have any of the components listed in the `not`array.
 ```javascript
-ecs.system({//move entities with velocity
+ecs.registerSystem({//move entities with velocity
   components: ["position", "velocity"],
-  every: function(pos, vel) {//iterates all entities with position and velocity component
+  forEach(position, velocity) {//iterates all entities with position and velocity component
     pos.x += vel.x;
     pos.y += vel.y;
   }
 });
 ```
+Use `not` array to exclude entities from the system. Below example shows how to render only entities that have position, sprite component and don't have `hidden` component. Another approach would be temporarily removing the sprite or position component.
+```javascript
+//Assuming there's a canvas and context for the sake of example
+ecs.registerSystem({
+  has: ["position", "sprite"],
+  not: ["hidden"],
+  pre() {/* called once before iterating all entities */
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  },
+  forEach(position, sprite) {
+    ctx.drawImage(sprites[sprite.name], position.x, position.y );
+  }
+});
+```
+Run all systems.
+```javascript
+ecs.run();
+```
+Systems can be run in groups and system within a group can be ordered.
+```javascript
+ecs.registerSystem({
+  pre() {
+    console.log("Calculating physics...");
+  },
+  order: 1,
+  group: "model"
+});
+
+ecs.registerSystem({
+  pre() {
+    console.log("Handling player input...");
+  },
+  order: 0,
+  group: "model"
+});
+
+ecs.registerSystem({
+  pre() {
+    console.log("Rendering a new frame...");
+  },
+  group: "graphics"
+});
+
+ecs.runGroup("model");
+//--> Handling player input...
+//--> Calculating physics...
+ecs.runGroup("graphics");
+//--> Rendering a new frame...
+```
+Pass a global argument to system call.
+```javascript
+ecs.registerSystem({
+  has: ["position", "velocity"],
+  forEach(position, velocity, entity, dt) {
+    position.x += velocity.x * dt;
+    position.y += velocity.y * dt;
+  },
+  group: "model",
+});
+
+//Within a game loop...
+const dt = 16;
+ecs.run(dt);//delta time value is typically calculated inside a game loop
+ecs.runGroup("model", dt);//global argument can be used in group run calls, too
+```
+## General findings and misc
 Components with constructors can be optionally used although it is slightly against the paradigm of Ecs.
 ```javascript
 function Vector(x, y) {
@@ -95,113 +166,13 @@ Vector.prototype.add = function(v) {
   this.y += v.y;
 }
 
-ecs.component("position", Vector);
-ecs.component("velocity", Vector);
+ecs.registerComponent("position", Vector);
+ecs.registerComponent("velocity", Vector);
 
-ecs.system({
+ecs.registerSystem({
   components: ["position", "velocity"],
-  every: function(pos,vel) {
+  forEach(pos, vel, entity) {
     pos.add(vel);//calling "add" method from position component which is instanceof Vector
-  }
-});
-```
-Use "not" array to exclude entities from the system.
-```javascript
-ecs.system({//sprite rendering system
-  components: ["position", "sprite"],
-  not: ["hidden"],
-  pre: function() {//called once before iterating all entities
-    ctx.clearRect(0,0,1000,1000);
-  },
-  every: function(pos, sprite) {
-    renderModule.drawSprite(sprite.src, pos.x, pos.y);//example function call, renderModule is not part of Ecs...
-  }
-});
-```
-Systems have `onenter` and `onleave` events for entities joining and leaving the system as a result of adding or removing components.
-```javascript
-ecs.system({
-  components: ["name", "talkingToPhone"],
-  onenter: function(name, talking) {
-    console.log("Hello, it's "+ name.full);
-  },
-  every: function(name, talking) {
-    console.log("bla bla bla...");
-  },
-  onleave: function(name, talking) {
-    console.log("Bye!");
-  }
-});
-```
-Systems have an event bus to communicate with other systems.
-```javascript
-ecs.system({
-  init: function() {//called once system is created
-    var system = this;
-    document.addEventListener("click", function(e) {
-      var msg = "Hello!";
-      system.emit("some_event", msg);
-    });
-  }
-});
-
-ecs.system({
-  init: function () {
-    this.on("some_event",function(e) {
-      console.log(e);//clicking will print "Hello!"
-    })
-  }
-});
-```
-Run all systems.
-```javascript
-ecs.run();
-```
-Systems can be run in groups.
-```javascript
-ecs.system({
-  pre: function () {
-    console.log("Running a system.");
-  },
-  group: "somegroup"
-});
-
-ecs.system({
-  pre: function () {
-    console.log("Running another system.");
-  },
-  group: "somegroup"
-});
-
-ecs.runGroup("somegroup");
-```
-Pass a global argument to system call.
-```javascript
-ecs.system({
-  components: ["position", "velocity"],
-  every: function(pos, vel, ent, dt) {
-    pos.x += vel.x * dt;
-    pos.y += vel.y * dt;
-  }
-});
-
-var dt = 16;
-ecs.run(dt);//typically inside a game loop
-ecs.runGroup("model", dt);//global argument can be used in group run calls, too
-```
-Each system maintain an array of entities matching the components listed in the `components` array.
-```javascript
-ecs.system({
-  components: ["position", "velocity"],
-  every: function(pos, vel, ent) {
-    //"this" refers to the system
-    //this.entities is an array of entities matching this system's components array
-    this.iterate(function(entity) {
-      //iterating this.entities array
-    });
-    this.iterateOthers(function(other) {
-      //iterating this.entities array except for the entity ("ent") on which the "every" method is called
-    });
   }
 });
 ```
