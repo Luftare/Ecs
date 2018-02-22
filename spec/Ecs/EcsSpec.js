0d14412e2a1f22e2f8c639d976fda0dfef83e764
Ilmari Koskinen <ilmari.koskinen@houston-inc.com>
2018-02-22 13:19:48 +0200

    Improve test description readability and add new tests for multiple entities

--- spec/Ecs/EcsSpec.js
+++ spec/Ecs/EcsSpec.js
@@ -47,7 +47,7 @@ describe('When components and systems are defined', () => {
     });
   });
 
-  describe('and when entity is created', () => {
+  describe('and entity is created', () => {
     let entity;
 
     beforeEach(() => {
@@ -65,7 +65,7 @@ describe('When components and systems are defined', () => {
       expect(entity).not.toBeInSystem(systems.render);
     });
 
-    describe('and when component is added to an entity', () => {
+    describe('and component is added to an entity', () => {
       beforeEach(() => {
         entity.add('position', 5, 5);
       });
@@ -87,7 +87,7 @@ describe('When components and systems are defined', () => {
         expect(entity.position).not.toBeDefined();
       });
 
-      describe('and when required components for systems are added', () => {
+      describe('and required components for systems are added', () => {
         beforeEach(() => {
           entity.add('velocity', 2, 2);
         });
@@ -104,7 +104,7 @@ describe('When components and systems are defined', () => {
           expect(entity.position.y).toEqual(9);
         });
 
-        describe('and when rejected (included in "not" array of system) component is added', () => {
+        describe('and rejected (included in "not" array of system) component is added', () => {
           beforeEach(() => {
             entity.add('stunned');
           });
@@ -118,7 +118,7 @@ describe('When components and systems are defined', () => {
           });
         });
 
-        describe('and when required component is removed', () => {
+        describe('and required component is removed', () => {
           beforeEach(() => {
             entity.remove('velocity');
           });
@@ -135,19 +135,73 @@ describe('When components and systems are defined', () => {
     });
   });
 
-  describe('and when multiple entities are created', () => {
+  describe('and multiple entities are created', () => {
     let entities;
     const entityCount = 10;
     beforeEach(() => {
       entities = [...Array(entityCount)].map(() => ecs.createEntity());
     });
 
-    it('systems should enroll only matchin entities', () => {
+    it('systems should enroll only matching entities', () => {
       expect(systems.noop).toMatchNumberOfEntities(entityCount);
       expect(systems.move).toMatchNumberOfEntities(0);
       expect(systems.render).toMatchNumberOfEntities(0);
     });
 
-  });
+    describe('and 2 entities receive 2 components in chain', () => {
+      beforeEach(() => {
+        entities[0].add('position', 5, 5).add('velocity', 3, 3);
+        entities[1].add('position', 5, 5).add('velocity', 3, 3);
+      });
 
+      it('entities should store component data', () => {
+        expect(entities[0].has('position')).toEqual(true);
+        expect(entities[1].has('position')).toEqual(true);
+        expect(entities[0].has('velocity')).toEqual(true);
+        expect(entities[1].has('velocity')).toEqual(true);
+        expect(entities[0].position).toBeDefined();
+        expect(entities[1].position).toBeDefined();
+        expect(entities[0].velocity).toBeDefined();
+        expect(entities[1].velocity).toBeDefined();
+        expect(entities[0].velocity.x).toEqual(3);
+        expect(entities[0].velocity.y).toEqual(3);
+        expect(entities[1].velocity.x).toEqual(3);
+        expect(entities[1].velocity.y).toEqual(3);
+      });
+
+      it('systems should enroll only matching entities', () => {
+        expect(systems.noop).toMatchNumberOfEntities(entityCount);
+        expect(systems.move).toMatchNumberOfEntities(2);
+        expect(systems.render).toMatchNumberOfEntities(0);
+        entities.forEach((entity, i) => {
+          expect(entity).not.toBeInSystem(systems.render);
+          expect(entity).toBeInSystem(systems.noop);
+          if(i < 2) {
+            expect(entity).toBeInSystem(systems.move);
+          } else {
+            expect(entity).not.toBeInSystem(systems.move);
+          }
+        });
+      });
+
+      it('systems should be able to process enrolled entities', () => {
+        ecs.run();
+        expect(entities[0].position.x).toEqual(8);
+        expect(entities[0].position.y).toEqual(8);
+        expect(entities[1].position.x).toEqual(8);
+        expect(entities[1].position.y).toEqual(8);
+      });
+
+      describe('and 1 entity receives rejected component', () => {
+        beforeEach(() => {
+          entities[0].add('stunned');
+        });
+
+        it('should be rejected from corresponding system', () => {
+          expect(entities[0]).not.toBeInSystem(systems.move);
+          expect(systems.move).toMatchNumberOfEntities(1);
+        });
+      });
+    });
+  });
 });
